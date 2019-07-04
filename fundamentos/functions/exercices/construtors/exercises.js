88e39d4dfbba67907f6b5e9286ecd10e92ab76a1
Dheyson Alves <dheyson.lucas@hotmail.com>
2019-07-04 00:01:14 -0300

    Adiciona mais exercicios

--- fundamentos/functions/exercices/construtors/exercises.js
+++ fundamentos/functions/exercices/construtors/exercises.js
@@ -1,16 +1,36 @@
 // TASK 1 - TWO FUNCTIONS - ONE OBJECT
 
-function A() {  }
-function B() {  }
+// function A() {  }
+// function B() {  }
 
-let a = new A;
-let b = new B;
+// let a = new A;
+// let b = new B;
 
-console.log(a == b); // true
+// console.log(a == b); // true
 
-let obj = {};
+// let obj = {};
 
-function A() { return obj; }
-function B() { return obj; }
+// function A() { return obj; }
+// function B() { return obj; }
 
-alert(new A() == new B()); // true
+// alert(new A() == new B()); // true
+
+
+// TASK 2 -- NEW CALCULATOR
+
+function Calculator (a = 5, b = 7) {
+
+    this.sum = function () {
+        return a + b
+    }
+
+    this.mul = function () {
+        return a * b
+    }
+
+}
+
+let calculator = new Calculator(8, 7)
+calculator.sum()
+
+console.log(calculator.mul())
