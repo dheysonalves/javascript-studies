64c1bb3b8f5215ad9cd54c6c057071b7192a88ef
Dheyson Alves <dheyson.lucas@hotmail.com>
2019-08-29 17:06:43 -0300

    Adiciona sobre ajax e aulas do Rocketseat

---
+++ fundamentos/ajax/ajax/server.js
@@ -0,0 +1,10 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/teste', (req, res) => { res.send('ok')})
+app.listen(8080, () => console.log('Executando...'))
