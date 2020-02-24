93c93ea50acac0cfb6fd712c3cb09500e50f499b
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-02-24 15:41:52 -0300

    Adiciona arquivo de debug do vscode

---
+++ fundamentos/callback/index.js
@@ -0,0 +1,8 @@
+const num = 3;
+function multiplyBy2(input) {
+  const result = input * 2;
+  return result;
+}
+
+const output = multiplyBy2(4);
+const newOutput = multiplyBy2(num);
---
+++ nodejs/basicos/debug/index.js
@@ -0,0 +1,6 @@
+var i = 0;
+
+setInterval(() => {
+  console.log("iteration", i);
+  i++;
+}, 1000);
